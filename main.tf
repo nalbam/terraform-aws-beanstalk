e444727cb5c9119c53055b86068f621b298ecbdf
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2019-05-30 12:09:53 +0900

    terraform 0.12

--- main.tf
+++ main.tf
@@ -1,13 +1,5 @@
 # Elastic Beanstalk
 
-terraform {
-  required_version = ">= 0.12"
-}
-
-provider "aws" {
-  region = var.region
-}
-
 resource "aws_elastic_beanstalk_application" "default" {
   name = var.name
 }
